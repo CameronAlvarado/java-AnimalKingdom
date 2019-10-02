9c35465eb0851f7388d0eeb036b274c05cc7c4a6
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-10-02 14:03:23 -0700

    got abstract class and other classes to compile. Working on collection now

---
+++ src/animalKing/Main.java
@@ -0,0 +1,22 @@
+package animalKing;
+
+import java.util.*;
+
+public class Main
+{
+	public static ArrayList<Animals> filteredList = new ArrayList<>();
+
+	// public static void printAniamls(ArrayList<Animals> animals, CheckAnimals tester)
+	// {
+	// 	filteredList.clear();
+
+	// 	for (Animals a : animals)
+	// 	{
+	// 		if (tester.test(a))
+	// 		{
+	// 			System.out.println(v.getName() + " " + v.getFood());
+	// 			filteredList.add(a);
+	// 		}
+	// 	}
+	// }
+}
